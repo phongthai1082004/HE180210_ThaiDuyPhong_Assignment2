d8d26d41a0228bc8795d7bcca79a727d8bfc3d59
Phong Thái <phongthai1082004@gmail.com>
2025-06-23 04:15:00 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HE180210_ThaiDuyPhong_Assignment2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
